35832ca58de5692c78a5f16a11de16b59dc6d43b
martin <martin.kosi@gmail.com>
2026-04-24 12:24:23 +0200

    register_autologin

--- app/src/main/java/com/example/myapplication/MyApplication.java
+++ app/src/main/java/com/example/myapplication/MyApplication.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.app.Application;
+import androidx.appcompat.app.AppCompatDelegate;
 import com.example.myapplication.util.Prefs;
 
 public class MyApplication extends Application {
@@ -8,5 +9,8 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Prefs.init(this);
+        
+        // Force Light Theme as default
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
